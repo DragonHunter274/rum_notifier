f23e053aa33b84d7494df86cdf3a880e062e2c66
DragonHunter274 <schurgel+github@gmail.com>
2024-07-08 09:11:17 +0000

    various fixes

--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import os
 import requests
 import re
 import time
+import pickle
+import time
 from apprise import Apprise
+
+# File to persist notified products
+PERSISTENCE_FILE = 'notified_products.pkl'
 
 # Function to fetch data from the API
 def fetch_data(api_url):
@@ -25,40 +30,60 @@ def send_notification(name, price, days_left):
     apobj = Apprise()
     # Add notification service URLs from the environment variable
     apprise_urls = os.getenv('APPRISE_URLS')
-    print(apprise_urls)
     if apprise_urls:
         for url in apprise_urls.split(','):
             apobj.add(url.strip())
     
-    message = f"Rum ist die naechsten {days_left} Tage bei {name} fuer {price}EUR im Angebot!"
+    title = "" 
+    body = f"Rum ist die naechsten {days_left} bei {name} fuer {price} im Angebot!" 
+
     apobj.notify(
-        body=message,
-        title=""
+        body=body,
+        title=title
     )
 
+# Function to load notified products from a file
+def load_notified_products(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'rb') as file:
+            return pickle.load(file)
+    return set()
+
+# Function to save notified products to a file
+def save_notified_products(notified_products, file_path):
+    with open(file_path, 'wb') as file:
+        pickle.dump(notified_products, file)
+
 # Main function
-def main(api_url, check_interval=3600):
-    notified_products = set()
+def main(check_interval=3600):
+    api_url = os.getenv('API_URL')
+    if not api_url:
+        raise ValueError("API_URL environment variable is not set")
+    notified_products = load_notified_products(PERSISTENCE_FILE)
     
     while True:
         data = fetch_data(api_url)
+        current_sales = set()
         
         for item in data:
             if item['status'] == 'im angebot':
                 price, days_left = parse_raw_data(item['raw'])
-                product_identifier = (item['name'], price, days_left)
+                product_identifier = (item['name'], price)
+                current_sales.add(product_identifier)
                 
                 if price and days_left and product_identifier not in notified_products:
                     send_notification(item['name'], price, days_left)
                     notified_products.add(product_identifier)
         
+        # Remove products from notified_products if their sale has ended
+        notified_products = notified_products.intersection(current_sales)
+        save_notified_products(notified_products, PERSISTENCE_FILE)
+        
         # Wait for the next check
         time.sleep(check_interval)
 
-# URL of your API
-api_url = 'https://rum.dh274.com'
 
 # Run the main function
 if __name__ == '__main__':
-    main(api_url)
+    main()
 
